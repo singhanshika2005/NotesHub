40755b6562202c0ca3f12682b0dd5cc070dedef5
Anshika Singh <as5204443@gmail.com>
2026-04-12 13:02:30 +0530

    BaseUrl Changes

--- src/BaseUrls.js
+++ src/BaseUrls.js
@@ -1,3 +1,3 @@
 
-export const BaseUrls = "http://localhost:8000/api/v3.2"
-// export const BaseUrls = "https://itsnotebook.onrender.com/api/v3.2"
+// export const BaseUrls = "http://localhost:8000/api/v3.2"
+export const BaseUrls = "https://itsnotebook.onrender.com/api/v3.2"
